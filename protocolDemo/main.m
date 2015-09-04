2e2265a8cb3723114c4f066ca1fe4416915739de
read <hedegang@icloud.com>
2015-09-04 18:50:32 +0800

    first

---
+++ protocolDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  protocolDemo
+//
+//  Created by read on 15/9/4.
+//  Copyright (c) 2015 ___read___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+
+    return 0;
+}
